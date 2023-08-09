64c7c8bbb96e6de93c200794183b0607b3694339
Victória Fernanda <vicvaldirene@gmail.com>
2023-08-09 14:01:59 -0300

    Victória

---
+++ PotencialEleletrico.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Charge:
+    def __init__(self, position, value, color):
+        self.position = position
+        self.value = value
+        self.color = color 
+        
+class ElectricFieldApp:
+    def __init__(self):
+        self.charges = []
+        self.colors = ['red', 'blue', 'green', 'purple']
+        self.fig, self.ax = plt.subplots()
+        self.ax.set_aspect('equal')
+        self.ax.set_xlim(-10, 10)
+        self.ax.set_ylim(-10, 10)
+        self.ax.set_xlabel('X')
+        self.ax.set_ylabel('Y')
+        self.ax.grid(True)
+        self.cid = self.fig.canvas.mpl_connect('button_press_event', self.on_click)
+        self.selected_point = None
